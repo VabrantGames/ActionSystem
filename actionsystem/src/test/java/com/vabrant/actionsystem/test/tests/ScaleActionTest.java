38ad316b8dcf61a8b655c3ab6891e25b7428c586
john <john.developer.mail@gmail.com>
2020-04-08 13:00:14 -0700

    ScaleAction refactored

--- actionsystem/src/test/java/com/vabrant/actionsystem/test/tests/ScaleActionTest.java
+++ actionsystem/src/test/java/com/vabrant/actionsystem/test/tests/ScaleActionTest.java
@@ -17,10 +17,15 @@ package com.vabrant.actionsystem.test.tests;
 
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Interpolation;
-import com.vabrant.actionsystem.actions.Action;
+import com.badlogic.gdx.math.MathUtils;
+import com.vabrant.actionsystem.actions.ActionAdapter;
+import com.vabrant.actionsystem.actions.ActionListener;
 import com.vabrant.actionsystem.actions.DelayAction;
 import com.vabrant.actionsystem.actions.GroupAction;
+import com.vabrant.actionsystem.actions.MoveAction;
+import com.vabrant.actionsystem.actions.RunnableAction;
 import com.vabrant.actionsystem.actions.ScaleAction;
 import com.vabrant.actionsystem.test.TestObject;
 
@@ -32,24 +37,48 @@ import space.earlygrey.shapedrawer.ShapeDrawer;
  */
 public class ScaleActionTest extends ActionSystemTestListener {
 
-	private TestObjectController testObjectController;
+	private float startX;
+	private float startY;
+	private float endX;
+	private float endY;
+	private float amountX;
+	private float amountY;
+	private TestObject testObject;
+	private ShapeRenderer renderer;
+	private ActionListener<ScaleAction> listener;
 	
-	public ScaleActionTest() {
-		testObjectController = TestObjectController.getInstance();
+	@Override
+	public void create() {
+		super.create();
+		testObject = new TestObject();
+		renderer = new ShapeRenderer();
+		renderer.setAutoShapeType(true);
+		shapeDrawer.setDefaultSnap(false);
+		shapeDrawer.setPixelSize(2);
+		listener = createListener();
+		reset();
 	}
-	
-	private TestObject getTestObject() {
-		TestObject object = testObjectController.create();
-		object.setSize(100, 100);
-		testObjectController.center(object, viewport);
-		return object;
+
+	private void reset() {
+		testObject.setScaleX(1);
+		testObject.setScaleY(1);
+		TestObjectController.getInstance().center(testObject, viewport);
 	}
-	
-	private GroupAction wrap(Action<?> a) {
-		return GroupAction.sequence(
-				DelayAction.delay(0.5f),
-				a,
-				DelayAction.delay(1f));
+
+	private ActionListener<ScaleAction> createListener(){
+		return new ActionAdapter<ScaleAction>() {
+			@Override
+			public void actionEnd(ScaleAction a) {
+				System.out.println();
+				System.out.println(a.getName());
+				System.out.println("StartX: " + startX);
+				System.out.println("StartY: " + startY);
+				System.out.println("EndX: " + endX);
+				System.out.println("EndY: " + endY);
+				System.out.println("X: " + testObject.getScaleX());
+				System.out.println("Y: " + testObject.getScaleY());
+			}
+		};
 	}
 	
 	@Override
@@ -72,40 +101,68 @@ public class ScaleActionTest extends ActionSystemTestListener {
 	}
 	
 	public void scaleXByTest() {
-		TestObject object = getTestObject();
-		ScaleAction action = ScaleAction.scaleXBy(object, -0.5f, 1f, Interpolation.linear);
-		GroupAction wrap = wrap(action);
-		wrap.addListener(object);
-		actionManager.addAction(wrap);
+		reset();
+		
+		amountX = 0.5f;
+		startX = testObject.getScaleX();
+		endX = startX + amountX;
+		startY = endY = testObject.getScaleY();
+		
+		actionManager.addAction(
+				ScaleAction.scaleXBy(testObject, amountX, 0.5f, Interpolation.linear)
+				.setName("ScaleXBy")
+				.addListener(listener));
 	}
-	
+
 	public void scaleYByTest() {
-		TestObject object = getTestObject();
-		ScaleAction action = ScaleAction.scaleYBy(object, 0.5f, 1f, Interpolation.linear);
-		GroupAction wrap = wrap(action);
-		wrap.addListener(object);
-		actionManager.addAction(wrap);
+		reset();
+		
+		amountY = -0.5f;
+		startY = testObject.getScaleY();
+		endY = startY + amountY;
+		startX = endX = testObject.getScaleX();
+		
+		actionManager.addAction(
+				ScaleAction.scaleYBy(testObject, amountY, 0.5f, Interpolation.linear)
+				.setName("ScaleYBy")
+				.addListener(listener));
 	}
 	
 	public void scaleXToTest() {
-		TestObject object = getTestObject();
-		ScaleAction action = ScaleAction.scaleXTo(object, 2, 1f, Interpolation.linear);
-		GroupAction wrap = wrap(action);
-		wrap.addListener(object);
-		actionManager.addAction(wrap);
+		reset();
+		
+		startX = testObject.getScaleX();
+		endX = 2;
+		endY = startY = testObject.getScaleY();
+		
+		actionManager.addAction(
+				ScaleAction.scaleXTo(testObject, endX, 0.5f, Interpolation.linear)
+				.setName("ScaleXTo")
+				.addListener(listener));
 	}
 	
 	public void scaleYToTest() {
-		TestObject object = getTestObject();
-		ScaleAction action = ScaleAction.scaleYTo(object, 2, 1f, Interpolation.linear);
-		GroupAction wrap = wrap(action);
-		wrap.addListener(object);
-		actionManager.addAction(wrap);
+		reset();
+		
+		startY = testObject.getScaleY();
+		endY = 3;
+		endX = startX = testObject.getScaleY();
+		
+		actionManager.addAction(
+				ScaleAction.scaleYTo(testObject, endY, 0.5f, Interpolation.linear)
+				.setName("ScaleYTo")
+				.addListener(listener));
 	}
-	
+
 	@Override
 	public void draw(SpriteBatch batch, ShapeDrawer shapeDrawer) {
-		testObjectController.draw(shapeDrawer);
+		batch.end();
+		
+		renderer.begin();
+		testObject.draw(renderer);
+		renderer.end();
+		
+		batch.begin();
 	}
 	
 }
