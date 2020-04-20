71fd62178a6dc8e424a5b86e3434bcd967cf35c5
john <john.developer.mail@gmail.com>
2020-04-19 18:51:17 -0700

    ActionLogger refactored

--- actionsystem/src/main/java/com/vabrant/actionsystem/actions/ActionLogger.java
+++ actionsystem/src/main/java/com/vabrant/actionsystem/actions/ActionLogger.java
@@ -2,12 +2,13 @@ package com.vabrant.actionsystem.actions;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.ObjectSet;
 import com.badlogic.gdx.utils.StringBuilder;
 
 public class ActionLogger {
 	
-	private static boolean useSysOut = false;
-	private static boolean isRelease = false;
+	private static boolean USE_SYS_OUT = false;
+	private static boolean RESTRICT_OUTPUT = false;
 	
 	public static final int NONE = 0;
 	public static final int ERROR = 1;
@@ -15,57 +16,44 @@ public class ActionLogger {
 	public static final int DEBUG = 3;
 	
 	private static final String EMPTY_STRING = ""; 
-	
-	private static final StringBuilder STRING_BUILDER;	
-	private static ActionLogger soloLogger;
-	
-	static {
-		if(!isRelease) {
-			STRING_BUILDER = new StringBuilder(50);
-		}
-		else {
-			STRING_BUILDER = null;
-		}
-	}
-	
+	private static final StringBuilder STRING_BUILDER = new StringBuilder(50);	
+	private static final ObjectSet<ActionLogger> SOLO_LOGGERS = new ObjectSet<>(10);
+
 	public static ActionLogger getLogger(Class<?> c) {
 		return getLogger(c, NONE);
 	}
 	
 	public static ActionLogger getLogger(Class<?> c, int level) {
-		return isRelease ? null : new ActionLogger(c, level);
+		return getLogger(c, null, level);
 	}
 	
-	public static void setReleaseMode(boolean releaseMode) {
-		ActionLogger.isRelease = releaseMode;
+	public static ActionLogger getLogger(Class<?> c, String name, int level) {
+		return new ActionLogger(c, name, level);
+	}
+	
+	public static void restrictOutput(boolean restrict) {
+		RESTRICT_OUTPUT = restrict;
 	}
 	
 	public static void useSysOut() {
-		useSysOut = true;
+		USE_SYS_OUT = true;
 	}
-	
-	public static void solo(ActionLogger logger) {
-		if(logger == null) return;
-		soloLogger = logger;
-	}
-	
+
+	private boolean solo;
+	private int level;
 	private String actionName;
 	private String className;
-	private int level;
 	
-	private ActionLogger(Class<?> c) {
-		this(c, NONE);
+	private ActionLogger() {}
+
+	private ActionLogger(Class<?> c, String name, int level) {
+		className = c.getSimpleName();
+		setName(name);
+		this.level = level;
 	}
 	
-	private ActionLogger(Class<?> c, int level) {
-		if(!isRelease) {
-			className = c.getSimpleName();
-			this.level = level;
-		}
-	}
-	
-	public void setActionName(String actionName) {
-		if(actionName == null || actionName.isEmpty()) return;
+	public void setName(String actionName) {
+		if(RESTRICT_OUTPUT || actionName == null || actionName.isEmpty()) return;
 		STRING_BUILDER.clear();
 		STRING_BUILDER.append('(');
 		STRING_BUILDER.append(actionName);
@@ -94,7 +82,7 @@ public class ActionLogger {
 	}
 	
 	public void info(String message, String body) {
-		if(isRelease || level < INFO) return;
+		if(RESTRICT_OUTPUT || level < INFO) return;
 		print(message, body, INFO);
 	}
 	
@@ -103,7 +91,7 @@ public class ActionLogger {
 	}
 	
 	public void debug(String message, String body) {
-		if(isRelease || level < DEBUG) return;
+		if(RESTRICT_OUTPUT || level < DEBUG) return;
 		print(message, body, DEBUG);
 	}
 	
@@ -112,19 +100,34 @@ public class ActionLogger {
 	}
 	
 	public void error(String message, String body) {
-		if(isRelease || level < ERROR) return;
+		if(RESTRICT_OUTPUT || level < ERROR) return;
 		print(message, body, ERROR);
 	}
 	
-	public void solo() {
-		if(isRelease) return;
-		ActionLogger.solo(this);
+	public void solo(boolean solo) {
+		if(RESTRICT_OUTPUT) return;
+		
+		this.solo = solo;
+		
+		if(solo) {
+			SOLO_LOGGERS.add(this);
+		}
+		else {
+			SOLO_LOGGERS.remove(this);
+		}
+	}
+	
+	public void reset() {
+		clearActionName();
+		level = NONE;
+		solo(false);
 	}
 	
 	private void print(String message, String body, int level) {
-		if(soloLogger != null && !soloLogger.equals(this)) return;
+		if(SOLO_LOGGERS.size > 0 && !solo) return;
 		
 		STRING_BUILDER.clear();
+		
 		if(actionName != null) {
 			STRING_BUILDER.append(actionName);
 			STRING_BUILDER.append(' ');
@@ -137,28 +140,34 @@ public class ActionLogger {
 			STRING_BUILDER.append(body);
 		}
 		
+		if(USE_SYS_OUT) {
+			STRING_BUILDER.insert(0, "[");
+			STRING_BUILDER.insert(1, className);
+			STRING_BUILDER.insert(1 + className.length(), "] ");
+		}
+		
 		switch(level) {
 			case ERROR:
+				if(!USE_SYS_OUT) {
+					Gdx.app.error(className, STRING_BUILDER.toString());
+				}
+				else {
+					System.out.println(STRING_BUILDER.toString());
+				}
 				break;
 			case INFO:
-				if(!useSysOut) {
+				if(!USE_SYS_OUT) {
 					Gdx.app.log(className, STRING_BUILDER.toString());
 				}
 				else {
-					STRING_BUILDER.insert(0, "[");
-					STRING_BUILDER.insert(1, className);
-					STRING_BUILDER.insert(1 + className.length(), "] ");
 					System.out.println(STRING_BUILDER.toString());
 				}
 				break;
 			case DEBUG:
-				if(!useSysOut) {
+				if(!USE_SYS_OUT) {
 					Gdx.app.debug(className, STRING_BUILDER.toString());
 				}
 				else {
-					STRING_BUILDER.insert(0, "[");
-					STRING_BUILDER.insert(1, className);
-					STRING_BUILDER.insert(1 + className.length(), "] ");
 					System.out.println(STRING_BUILDER.toString());
 				}
 				break;
---
+++ actionsystem/src/test/java/com/vabrant/actionsystem/test/tests/ActionLoggerTest.java
@@ -0,0 +1,78 @@
+/**
+ *	Copyright 2020 See AUTHORS file.
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ */
+package com.vabrant.actionsystem.test.tests;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.vabrant.actionsystem.actions.ActionLogger;
+import com.vabrant.actionsystem.test.ActionSystemTestConstantsAndUtils;
+
+/**
+ * @author John Barton
+ *
+ */
+public class ActionLoggerTest {
+	
+	private static ActionLogger logger;
+	
+	@BeforeClass
+	public static void init() {
+		ActionLogger.useSysOut();
+		logger = ActionLogger.getLogger(ActionLoggerTest.class, "MyLogger", ActionLogger.DEBUG);
+	}
+	
+	@Test 
+	public void basicTest() {
+		ActionSystemTestConstantsAndUtils.printTestHeader("Basic Test");
+		logger.info("Info", "Hello");
+		logger.debug("Debug", "World");
+		logger.error("Error", "Goodbye");
+	}
+	
+	@Test
+	public void resetTest() {
+		ActionSystemTestConstantsAndUtils.printTestHeader("Reset Test");
+		logger.info("Before Reset");
+		logger.reset();
+		
+		logger.setLevel(ActionLogger.DEBUG);
+		logger.info("After Reset");
+		
+		init();
+	}
+	
+	@Test
+	public void soloTest() {
+		ActionSystemTestConstantsAndUtils.printTestHeader("Solo Test");
+		
+		ActionLogger soloLogger = ActionLogger.getLogger(ActionLoggerTest.class, "Solo", ActionLogger.DEBUG);
+		
+		logger.info("Hello");
+		soloLogger.info("Hello");
+		
+		soloLogger.solo(true);
+		
+		logger.info("Should not be logged");
+		soloLogger.info("Solo me!!");
+		
+		soloLogger.solo(false);
+		
+		logger.info("Was i muted?");
+		
+	}
+
+}
