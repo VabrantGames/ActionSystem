06b9989bfeb12ba7aa7aeb33241767dd8c247895
VabrantGames <vabrantgames@gmail.com>
2023-05-26 18:25:25 -0700

    Sonatype setup. Minor cleanup

--- platformtests/android/src/main/java/com/vabrant/actionsystem/platformtests/android/FragmentTestStarter.java
+++ platformtests/android/src/main/java/com/vabrant/actionsystem/platformtests/android/FragmentTestStarter.java
@@ -14,123 +14,128 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.ListFragment;
-import com.vabrant.actionsystem.platformtests.tests.PlatformTest;
-import com.vabrant.actionsystem.platformtests.tests.PlatformTests;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.badlogic.gdx.backends.android.AndroidFragmentApplication;
+import com.vabrant.actionsystem.platformtests.tests.PlatformTest;
+import com.vabrant.actionsystem.platformtests.tests.PlatformTests;
 import com.vabrant.actionsystem.test.R;
-
 import java.util.List;
 
 public class FragmentTestStarter extends FragmentActivity implements AndroidFragmentApplication.Callbacks {
 
     FrameLayout list;
-	FrameLayout view;
+    FrameLayout view;
 
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		LinearLayout layout = new LinearLayout(this);
-		layout.setOrientation(LinearLayout.HORIZONTAL);
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.HORIZONTAL);
 
-		list = new FrameLayout(this);
-		list.setId(R.id.framelayout);
-		list.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-		layout.addView(list);
+        list = new FrameLayout(this);
+        list.setId(R.id.framelayout);
+        list.setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        layout.addView(list);
 
-		list.setLayoutParams(new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT, 1));
+        list.setLayoutParams(new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT, 1));
 
-		view = new FrameLayout(this);
-		view.setId(R.id.viewlayout);
-		view.setLayoutParams(new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT, 2));
-		layout.addView(view);
+        view = new FrameLayout(this);
+        view.setId(R.id.viewlayout);
+        view.setLayoutParams(new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT, 2));
+        layout.addView(view);
 
-		setContentView(layout);
+        setContentView(layout);
 
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction().add(R.id.framelayout, new TestListFragment()).commit();
-		}
-	}
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.framelayout, new TestListFragment())
+                    .commit();
+        }
+    }
 
-	public void onTestSelected (String testName) {
-		if (view != null) {
-			getSupportFragmentManager().beginTransaction().replace(R.id.viewlayout, TestViewFragment.newInstance(testName)).commit();
-		} else {
-			startActivity(new Intent(this, PlatformTestActivity.class).putExtra("test", testName));
-		}
-	}
+    public void onTestSelected(String testName) {
+        if (view != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.viewlayout, TestViewFragment.newInstance(testName))
+                    .commit();
+        } else {
+            startActivity(new Intent(this, PlatformTestActivity.class).putExtra("test", testName));
+        }
+    }
 
-	@Override
-	public void exit () {
-		//
-	}
+    @Override
+    public void exit() {
+        //
+    }
 
-	public static class TestListFragment extends ListFragment {
+    public static class TestListFragment extends ListFragment {
 
-		private SharedPreferences prefs;
-		private FragmentTestStarter activity;
+        private SharedPreferences prefs;
+        private FragmentTestStarter activity;
 
-		@Override
-		public void onCreate (Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			List<String> testNames = PlatformTests.getNames();
-			setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, testNames));
-			prefs = getActivity().getSharedPreferences("libgdx-tests", Context.MODE_PRIVATE);
-		}
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            List<String> testNames = PlatformTests.getNames();
+            setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, testNames));
+            prefs = getActivity().getSharedPreferences("libgdx-tests", Context.MODE_PRIVATE);
+        }
 
-		@Override
-		public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-			View view = super.onCreateView(inflater, container, savedInstanceState);
-			((ListView)view.findViewById(android.R.id.list)).setSelectionFromTop(prefs.getInt("index", 0), prefs.getInt("top", 0));
-			return view;
-		}
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View view = super.onCreateView(inflater, container, savedInstanceState);
+            ((ListView) view.findViewById(android.R.id.list))
+                    .setSelectionFromTop(prefs.getInt("index", 0), prefs.getInt("top", 0));
+            return view;
+        }
 
-		@Override
-		public void onListItemClick (ListView listView, View view, int position, long id) {
-			super.onListItemClick(listView, view, position, id);
+        @Override
+        public void onListItemClick(ListView listView, View view, int position, long id) {
+            super.onListItemClick(listView, view, position, id);
 
-			Object o = this.getListAdapter().getItem(position);
-			String testName = o.toString();
-			if (activity != null) {
-				activity.onTestSelected(testName);
-			}
-		}
+            Object o = this.getListAdapter().getItem(position);
+            String testName = o.toString();
+            if (activity != null) {
+                activity.onTestSelected(testName);
+            }
+        }
 
-		@Override
-		public void onAttach (Activity activity) {
-			super.onAttach(activity);
-			if (activity instanceof FragmentTestStarter) {
-				this.activity = (FragmentTestStarter)activity;
-			}
-		}
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            if (activity instanceof FragmentTestStarter) {
+                this.activity = (FragmentTestStarter) activity;
+            }
+        }
+    }
 
-	}
+    public static class TestViewFragment extends AndroidFragmentApplication {
 
-	public static class TestViewFragment extends AndroidFragmentApplication {
+        public static TestViewFragment newInstance(String testName) {
+            Bundle arguments = new Bundle();
+            arguments.putString("test", testName);
+            TestViewFragment fragment = new TestViewFragment();
+            fragment.setArguments(arguments);
+            return fragment;
+        }
 
-		public static TestViewFragment newInstance (String testName) {
-			Bundle arguments = new Bundle();
-			arguments.putString("test", testName);
-			TestViewFragment fragment = new TestViewFragment();
-			fragment.setArguments(arguments);
-			return fragment;
-		}
+        PlatformTest test;
 
-		PlatformTest test;
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            test = PlatformTests.newTest(getArguments().getString("test"));
+        }
 
-		@Override
-		public void onCreate (Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			test = PlatformTests.newTest(getArguments().getString("test"));
-		}
-
-		@Override
-		public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-			AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-			config.useImmersiveMode = true;
-			return initializeForView(test, config);
-		}
-
-	}
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+            config.useImmersiveMode = true;
+            return initializeForView(test, config);
+        }
+    }
 }
--- platformtests/android/src/main/java/com/vabrant/actionsystem/platformtests/android/PlatformTestActivity.java
+++ platformtests/android/src/main/java/com/vabrant/actionsystem/platformtests/android/PlatformTestActivity.java
@@ -5,10 +5,10 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 import androidx.annotation.Nullable;
-import com.vabrant.actionsystem.platformtests.tests.PlatformTest;
-import com.vabrant.actionsystem.platformtests.tests.PlatformTests;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.vabrant.actionsystem.platformtests.tests.PlatformTest;
+import com.vabrant.actionsystem.platformtests.tests.PlatformTests;
 
 public class PlatformTestActivity extends AndroidApplication {
 
@@ -16,23 +16,23 @@ public class PlatformTestActivity extends AndroidApplication {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-		// use the full display, even if we have a device with a notch
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-			Window applicationWindow = getApplicationWindow();
-			WindowManager.LayoutParams attrib = applicationWindow.getAttributes();
-			attrib.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
-		}
+        // use the full display, even if we have a device with a notch
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            Window applicationWindow = getApplicationWindow();
+            WindowManager.LayoutParams attrib = applicationWindow.getAttributes();
+            attrib.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        }
 
-		// obtain the test info
-		Bundle extras = getIntent().getExtras();
-		String testName = (String)extras.get("test");
-		PlatformTest test = PlatformTests.newTest(testName);
+        // obtain the test info
+        Bundle extras = getIntent().getExtras();
+        String testName = (String) extras.get("test");
+        PlatformTest test = PlatformTests.newTest(testName);
 
-		// and run the application...
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		config.useImmersiveMode = true;
-		config.useRotationVectorSensor = true;
-		config.useGyroscope = true;
-		initialize(test, config);
+        // and run the application...
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useImmersiveMode = true;
+        config.useRotationVectorSensor = true;
+        config.useGyroscope = true;
+        initialize(test, config);
     }
 }
